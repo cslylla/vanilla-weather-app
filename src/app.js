aa653398efaa7d472591d40507d233274e93223a
cslylla <81328857+cslylla@users.noreply.github.com>
2021-04-20 21:28:28 +0200

    JS: updated time and date

--- src/app.js
+++ src/app.js
@@ -1,15 +1,48 @@
-function displayTemperature(response){
-let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = Math.round(response.data.main.temp);
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturdayday", 
+    ]
+    let day = days[date.getDay()];
+    return `${day}`
+}
 
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML =response.data.name; 
-let descriptionElement = document.querySelector("#description");
-descriptionElement.innerHTML=response.data.weather[0].description;
-let humidityElement = document.querySelector("#humidity");
-humidityElement.innerHTML = response.data.main.humidity;
-let windElement = document.querySelector("#wind");
-windElement.innerHTML = Math.round(response.data.wind.speed);
+function formatTime(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (minutes<10){
+        minutes = `0${minutes}`
+    }
+    if (hours<10){
+        hours = `0${hours}`
+    }
+    return `${hours}:${minutes}`
+}
+
+function displayTemperature(response){
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML =response.data.name; 
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML=response.data.weather[0].description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML =formatDate(response.data.dt *1000);
+    let timeElement = document.querySelector("#time");
+    timeElement.innerHTML =formatTime(response.data.dt *1000);
+
 }
 
 let apiKey = "dae43417d2ff1d99a68e276b41145b89";
